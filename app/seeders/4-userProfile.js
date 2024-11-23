ca5eca66194f9441542ad645920f04a606253249
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-11-23 16:38:14 +0530

    Added the seeders and also seeder sile

---
+++ app/seeders/4-userProfile.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** @type {import("sequelize-cli").Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    return  queryInterface.bulkInsert("user_profiles", [
+      {
+        user_id : 1,
+        name : "Admin",
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        user_id : 2,
+        name : "User",
+        created_at : new Date(),
+        updated_at : new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete("People", null, {});
+     */
+  }
+};
+
+
---
+++ app/seeders/5-userRole.js
@@ -0,0 +1,34 @@
+"use strict";
+require("dotenv").config()
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+return queryInterface.bulkInsert("user_roles", 
+  [
+    {
+      user_id : 1,
+      role_id : 1,
+      created_at : new Date(),
+      updated_at : new Date()
+    },
+    {
+      user_id : 2,
+      role_id : 2,
+      created_at : new Date(),
+      updated_at : new Date()
+    }
+  ]);
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete("People", null, {});
+     */
+  }
+};
+
+
